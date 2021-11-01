09fc8a28551ff8da1c788d9f11feabf953f6e18b
dieuanhn <71412021+dieuanhn@users.noreply.github.com>
2021-11-01 12:29:09 -0400

    ??

--- my-project/src/components/options.vue
+++ my-project/src/components/options.vue
@@ -18,15 +18,40 @@ export default {
       colors: [
         {
         mainColor: "Red", 
-        image: require(), 
-        
-      }
-        
-      
+        image: require('../assets/main/mainred.PNG'), 
+        mainColor: "Blue", 
+        image: require('../assets/main/mainblue.PNG'), 
+        mainColor: "Pink", 
+        image: require('../assets/main/mainpink.PNG'),
+        mainColor: "Orange", 
+        image: require('../assets/main/mainorange.PNG'),
+        mainColor: "Green", 
+        image: require('../assets/main/maingreen.PNG'),
+        mainColor: "Purple", 
+        image: require('../assets/main/mainpurple.PNG'),
+        },
+
+        {
+        secondaryColor: "Red", 
+        image: require('../assets/main/mainred.PNG'), 
+        secondaryColor: "Blue", 
+        image: require('../assets/main/mainblue.PNG'), 
+        secondaryColor: "Pink", 
+        image: require('../assets/main/mainpink.PNG'),
+        secondaryColor: "Orange", 
+        image: require('../assets/main/mainorange.PNG'),
+        secondaryColor: "Green", 
+        image: require('../assets/main/maingreen.PNG'),
+        secondaryColor: "Purple", 
+        image: require('../assets/main/mainpurple.PNG'),
+        }
+
+     ]
+
+    
+
+  }
      
   }
-      ]
-     
-}
  
 </script>
