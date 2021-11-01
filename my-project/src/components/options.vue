129333c1b4c7c8fbee6fb87e08633b989ae85bdd
dieuanhn <71412021+dieuanhn@users.noreply.github.com>
2021-11-01 17:27:04 -0400

    help

--- my-project/src/components/options.vue
+++ my-project/src/components/options.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="option">
 <h2>
-Chose A Main Color: {{ mainColors }}
+ {{ mainColors }}
 </h2>
 </div> 
 
@@ -15,43 +15,98 @@ export default {
 
   data() {
     return {
-      colors: [
-        {
-        mainColor: "Red", 
-        image: require('../assets/main/mainred.PNG'), 
-        mainColor: "Blue", 
-        image: require('../assets/main/mainblue.PNG'), 
-        mainColor: "Pink", 
-        image: require('../assets/main/mainpink.PNG'),
-        mainColor: "Orange", 
-        image: require('../assets/main/mainorange.PNG'),
+    mainColors: [
+      { 
+        name: "Red", 
+        image: require('../assets/main/mainRed.PNG') 
+      }, 
+      {
+        name: "Blue", 
+        image: require('../assets/main/mainBlue.PNG')
+      }, 
+      {
+        name: "Pink", 
+        image: require('../assets/main/mainPink.PNG')
+      },
+      { 
+        name: "Orange", 
+        image: require('../assets/main/mainOrange.PNG')
+      },
+      {
         mainColor: "Green", 
-        image: require('../assets/main/maingreen.PNG'),
-        mainColor: "Purple", 
-        image: require('../assets/main/mainpurple.PNG'),
-        },
+        image: require('../assets/main/mainGreen.PNG')
+      },
+      {
+      mainColor: "Purple", 
+        image: require('../assets/main/mainPurple.PNG')
+      },
+     ], 
 
-        {
-        secondaryColor: "Red", 
-        image: require('../assets/main/mainred.PNG'), 
-        secondaryColor: "Blue", 
-        image: require('../assets/main/mainblue.PNG'), 
-        secondaryColor: "Pink", 
-        image: require('../assets/main/mainpink.PNG'),
-        secondaryColor: "Orange", 
-        image: require('../assets/main/mainorange.PNG'),
-        secondaryColor: "Green", 
-        image: require('../assets/main/maingreen.PNG'),
-        secondaryColor: "Purple", 
-        image: require('../assets/main/mainpurple.PNG'),
-        }
+    secondaryColors: [
+      { 
+        name: "Red", 
+        image: require('../assets/secondary/secondaryRed.PNG') 
+      }, 
+      {
+        name: "Blue", 
+        image: require('../assets/secondary/secondaryBlue.PNG')
+      }, 
+      {
+        name: "Pink", 
+        image: require('../assets/secondary/secondaryPink.PNG')
+      },
+      { 
+        name: "Orange", 
+        image: require('../assets/secondary/secondaryOrange.PNG')
+      },
+      {
+        mainColor: "Green", 
+        image: require('../assets/secondary/secondaryGreen.PNG')
+      },
+      {
+      mainColor: "Purple", 
+        image: require('../assets/secondary/secondaryPurple.PNG')
+      },
+     ], 
 
-     ]
+    accentColors: [
+      { 
+        name: "White", 
+        image: require('../assets/accent/accentWhite.PNG') 
+      }, 
+      {
+        name: "Black", 
+        image: require('../assets/accent/accentBlack.PNG')
+      }, 
+      {
+        name: "Grey", 
+        image: require('../assets/accent/accentGrey.PNG')
+      },
+     ], 
 
-    
-
+    wheelTypes: [
+      { 
+        name: "Hard", 
+        image: require('../assets/wheels/wheelsHard.PNG') 
+      }, 
+      {
+        name: "Intermediate", 
+        image: require('../assets/wheels/wheelsIntermediate.PNG')
+      }, 
+      {
+        name: "Medium", 
+        image: require('../assets/wheels/wheelsMedium.PNG')
+      },
+      { 
+        name: "Soft", 
+        image: require('../assets/wheels/wheelsSoft.PNG')
+      },
+      {
+        mainColor: "Wet", 
+        image: require('../assets/wheels/wheelsWet.PNG')
+      },
+     ],     
+    }
   }
-     
-  }
- 
+}
 </script>
